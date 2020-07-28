c00e72ff9242deb6cfdd91f8e3030398ed79fb47
r3dDoX <r3dDoX@users.noreply.github.com>
2020-07-28 14:29:36 +0200

    don't use write stream for single write

--- ledreset.js
+++ ledreset.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
-const ws = fs.createWriteStream('/dev/ledchain0');
 
 const LED_COUNT = 72;
 const BYTES_PER_LED = 3;
 
-const buffer = new Buffer(LED_COUNT * BYTES_PER_LED);
-ws.write(buffer);
-
+fs.writeFile('/dev/ledchain0', new Buffer(LED_COUNT * BYTES_PER_LED), (err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('reset');
+})
