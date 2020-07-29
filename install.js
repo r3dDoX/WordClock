314a4ed38ae1ade9a634e565e886d16b582ab727
Patrick Walther <patrick@walther-online.ch>
2020-07-29 12:44:47 +0200

    prevent printing of both messages

--- install.js
+++ install.js
@@ -5,6 +5,7 @@ if (os.arch() === 'mipsel' && os.hostname().startsWith('Omega-')) {
   exec('./scripts/install.sh', (error) => {
     if (error) {
       console.error('Failed to install ledchain');
+      return;
     }
 
     console.log('Successfully installed ledchain');
