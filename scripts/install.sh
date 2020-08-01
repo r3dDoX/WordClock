85cad39c1735ce8d581d95e17096ae49ffafe797
Patrick Walther <patrick@walther-online.ch>
2020-08-01 14:39:28 +0200

    only increase retries and no timeouts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 # intialize kernel module for WS2812B LED Strip
 rmmod p44-ledchain;
-insmod p44-ledchain ledchain0=0,150,1,10,5100;
+insmod p44-ledchain ledchain0=0,150,1,10;
 
 # Ensure PWM is pointing to pin 0
 omega2-ctrl gpiomux set pwm0 pwm;
